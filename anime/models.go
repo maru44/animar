86274e90b3a0620db29e5095fdb125e773cbbb36
maru44 <takumaru22244@gmail.com>
2021-05-05 21:22:11 +0900

    f

--- anime/models.go
+++ anime/models.go
@@ -3,22 +3,25 @@ package anime
 import (
 	"database/sql"
 	"fmt"
+	"os"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type Anime struct {
-	ID int
+	ID   int
 	Name string
 }
 
-const MysqlUser = os.Getenv("MYSQL_USER")
-const MysqlPassword = os.Getenv("MYSQL_PASSWORD")
-const MysqlDataBase = os.Getenv("MYSQL_DB")
+var MysqlUser = os.Getenv("MYSQL_USER")
+var MysqlPassword = os.Getenv("MYSQL_PASSWORD")
+var MysqlDataBase = os.Getenv("MYSQL_DB")
 
-func AccessDB() {
-	db, err := sql.Open("mysql", fmt.Fprintf("%v:%v@tcp(127.0.0.1:3306)/%v"), MysqlUser, MysqlPassword, MysqlDataBase)
+func AccessDB() *sql.DB {
+	db, err := sql.Open("mysql", fmt.Sprintf("%v:%v@tcp(http://localhost:3306)/%v", MysqlUser, MysqlPassword, MysqlDataBase))
 	if err != nil {
 		panic(err.Error())
 	}
-}
 
+	return db
+}
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
+	fmt.Println("Hello world!")
+	// http.HandleFunc("/",)
 
+	http.ListenAndServe(":8080", nil)
 }
