790a79d11853305cc5060f9d504176565c7894c0
maru44 <takumaru22244@gmail.com>
2021-05-18 21:42:28 +0900

    s3 uploader base

--- helper/bucket.go
+++ helper/bucket.go
@@ -1,13 +1,83 @@
 package helper
 
 import (
+	"bytes"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"google.golang.org/grpc/internal/credentials"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
-func S3Session() {
-	sess, err := session.Must(session.NewSession(&aws.Config{
-		Credentials: credentials.NewStaticCredentials("", "", ""),
-	}))
+func S3Session() *session.Session {
+	/*
+		sess, err := session.NewSessionWithOptions(session.Options{
+			Config: aws.Config{
+				Region: aws.String("ap-northeast-1"),
+				Credentials: aws.Config{
+					Credentials: credentials.NewStaticCredentialsFromCreds(credentials.Value{
+						os.Getenv("S3_ACCESS_KEY_ID"),
+						os.Getenv("S3_SECRET_KEY"),
+					}),
+				},
+			},
+			Profile: os.Getenv(""),
+		})
+	*/
+
+	creds := credentials.NewStaticCredentials(
+		os.Getenv("S3_ACCESS_KEY_ID"),
+		os.Getenv("S3_SECRET_KEY"),
+		"",
+	)
+	sess := session.Must(session.NewSession(
+		&aws.Config{
+			Credentials: creds,
+			Region:      aws.String("ap-northeast-1"),
+		},
+	))
+
+	return sess
+}
+
+func UploadS3(file []byte, fileName string) (string, error) {
+	contentType := getContentType(filepath.Ext(fileName))
+	if contentType == "" {
+		return "", errors.New("Unknown type")
+	}
+
+	sess := S3Session()
+	u := s3manager.NewUploader(sess)
+
+	key := fmt.Sprintf("test/%s", fileName)
+
+	_, err := u.Upload(&s3manager.UploadInput{
+		Body:        bytes.NewReader(file),
+		Bucket:      aws.String(os.Getenv("BUCKET")),
+		ContentType: aws.String(contentType),
+		Key:         aws.String(key),
+	})
+	if err != nil {
+		return "", err
+	}
+
+	fileUrl := fmt.Sprintf("https://s3-%s.amazonaws.com/%s/%s", "ap-northeast-1", os.Getenv("BUCKET"), key)
+	return fileUrl, nil
+}
+
+func getContentType(extension string) string {
+	switch extension {
+	case "jpg":
+		return "image/jpg"
+	case "jpeg":
+		return "image/jpg"
+	case "png":
+		return "image/png"
+	default:
+		return ""
+	}
 }
