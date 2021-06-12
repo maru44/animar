7d388aeb15f09c96f40be7c287e2c820361457d8
maru44 <takumaru22244@gmail.com>
2021-06-12 17:42:05 +0900

    cors

--- tools/api.go
+++ tools/api.go
@@ -112,7 +112,7 @@ func SetDefaultResponseHeader(w http.ResponseWriter) bool {
 	}
 	w.Header().Set("Access-Control-Allow-Origin", protocol+host)
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	//w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, Origin, X-Csrftoken, Accept, Cookie")
 	//w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT")
