861b74b9bce1aaf127b883e55a8a9ee5b289ce9b
maru44 <takumaru22244@gmail.com>
2021-07-22 22:05:19 +0900

    backup db

--- pkg/batch/database/database.go
+++ pkg/batch/database/database.go
@@ -4,7 +4,6 @@ import (
 	"animar/v1/configs"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os/exec"
 )
 
@@ -37,11 +36,26 @@ func BackupMainDatabase() {
 	}
 }
 
-func Sample() {
-	cmd := exec.Command("pwd")
-	if result, err := cmd.Output(); err != nil {
-		log.Print(err)
-	} else {
-		log.Print(string(result))
-	}
-}
+// func Bad() {
+// 	cmd := exec.Command(
+// 		"mysqldump", "--single-transaction", "--skip-lock-tables",
+// 		fmt.Sprintf("-u%s", configs.MysqlUser),
+// 		fmt.Sprintf("-p%s", configs.MysqlPassword),
+// 		fmt.Sprintf("%s", configs.MysqlDataBase),
+// 		fmt.Sprintf("> %s", backupFileName),
+// 	)
+// 	cmd.Stdout = os.Stdout
+// 	cmd.Stderr = os.Stderr
+// 	if err := cmd.Run(); err != nil {
+// 		log.Print(err)
+// 	}
+// }
+
+// func Sample() {
+// 	cmd := exec.Command("pwd")
+// 	if result, err := cmd.Output(); err != nil {
+// 		log.Print(err)
+// 	} else {
+// 		log.Print(string(result))
+// 	}
+// }
--- pkg/batch/main.go
+++ pkg/batch/main.go
@@ -4,5 +4,6 @@ import "animar/v1/pkg/batch/database"
 
 func main() {
 	database.BackupMainDatabase()
+	// database.Bad()
 	// database.Sample()
 }
