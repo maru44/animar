95fc7de1b86364b9e17d42e1b53140c1d72a09cf
maru44 <takumaru22244@gmail.com>
2021-07-29 23:18:20 +0900

    remove old logger

--- internal/pkg/domain/logger.go
+++ internal/pkg/domain/logger.go
@@ -5,7 +5,6 @@ import (
 	"animar/v1/internal/pkg/tools/tools"
 	"encoding/json"
 	"fmt"
-	"log"
 	"os"
 	"runtime"
 	"time"
@@ -51,13 +50,6 @@ type LogA struct {
 	Address string `json:"address"`
 	Method  string `json:"method"`
 	Path    string `json:"path"`
-}
-
-type SError struct {
-	Level   string    `json:"level"`
-	Content string    `json:"content"`
-	Place   string    `json:"place"`
-	Time    time.Time `json:"time"`
 }
 
 func NewAccessLog(addr, method, path string) *LogA {
@@ -126,45 +118,6 @@ func (e *LogE) write() {
 	defer logFile.Close()
 
 	writeJsonFile(fmt.Sprintf("%s/log_%s.json", configs.ErrorLogDirectory, today), e)
-}
-
-func ErrorLog(err error, mode string) {
-	if tools.IsProductionEnv() {
-		today := time.Now().Format("20060102")
-		logFile, _ := os.OpenFile(fmt.Sprintf("%s/log_%s.log", configs.ErrorLogDirectory, today), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
-		defer logFile.Close()
-
-		// get called place
-		_, file, line, _ := runtime.Caller(1)
-
-		log.SetOutput(logFile)
-		log.SetFlags(log.LstdFlags)
-
-		// set prefix
-		var level string
-		switch mode {
-		case "":
-			level = LogInfo
-		default:
-			level = mode
-		}
-		log.SetPrefix(level)
-
-		log.Println(fmt.Sprintf("%s:%d: %v", file, line, err))
-
-		/***************************
-		         json log
-		***************************/
-		e := &SError{
-			Level:   level,
-			Content: err.Error(),
-			Place:   fmt.Sprintf("%s:%d", file, line),
-			Time:    time.Now(),
-		}
-		writeJsonFile(fmt.Sprintf("%s/log_%s.json", configs.ErrorLogDirectory, today), e)
-	} else {
-		log.Print(err)
-	}
 }
 
 func writeJsonFile(fileName string, object interface{}) {
