251bb517a031fc250f9450099c72b8bcec30efe1
maru44 <takumaru22244@gmail.com>
2021-08-27 21:44:55 +0900

    article repository

--- internal/pkg/interfaces/database/article_repository.go
+++ internal/pkg/interfaces/database/article_repository.go
@@ -9,6 +9,32 @@ type ArticleRepository struct {
 	SqlHandler
 }
 
+/*   interactor   */
+// type ArticleInteractor interface {
+// 	FetchArticles() ([]Article, error)
+// 	GetArticleById(id int) (Article, error)
+// 	GetArticleBySlug(slug string) (Article, error)
+// 	InsertArticle(articleInput ArticleInput) (int, error)
+// 	UpdateArticle(articleInput ArticleInput, id int) (int, error)
+// 	DeleteArticle(id int) (int, error)
+// 	FetchArticleByAnime(animeId int) ([]Article, error)
+
+// 	// character
+
+// 	FetchArticleCharas(articleId int) ([]ArticleCharacter, error)
+// 	FetchArticleCharasByUser(userId string) ([]ArticleCharacter, error)
+// 	InsertArticleChara(charaInput ArticleCharacterInput) (int, error)
+// 	UpdateArticleChara(charaInput ArticleCharacterInput, id int) (int, error)
+// 	DeleteArticleChara(id int) (int, error)
+
+// 	// interview
+
+// 	FetchInterviewQuotes(articleId int) ([]InterviewQuote, error)
+// 	InsertInterviewQuote(interviewInput InterviewQuoteInput) (int, error)
+// 	UpdateInterviewQuote(interviewInput InterviewQuoteInput, id int) (int, error)
+// 	DeleteInterviewQuote(id int) (int, error)
+// }
+
 func (artr *ArticleRepository) Fetch() (articles []domain.Article, err error) {
 	rows, err := artr.Query(
 		"SELECT id, slug, article_type, abstract, content, image, author, is_public, user_id, created_at, updated_at, ",
@@ -36,7 +62,7 @@ func (artr *ArticleRepository) Fetch() (articles []domain.Article, err error) {
 	return
 }
 
-func (artr *ArticleRepository) GetArticleById(id int) (a domain.Article, err error) {
+func (artr *ArticleRepository) GetById(id int) (a domain.Article, err error) {
 	rows, err := artr.Query(
 		"SELECT id, slug, article_type, abstract, content, image, author, is_public, user_id, created_at, updated_at, ",
 		"FROM articles ",
@@ -61,6 +87,35 @@ func (artr *ArticleRepository) GetArticleById(id int) (a domain.Article, err err
 		} else {
 			a.Characters = cs
 		}
+	}
+	return
+}
+
+func (artr *ArticleRepository) GetBySlug(slug string) (a domain.Article, err error) {
+	rows, err := artr.Query(
+		"SELECT id, slug, article_type, abstract, content, image, author, is_public, user_id, created_at, updated_at, ",
+		"FROM articles ",
+		"WHERE slug = ?",
+		slug,
+	)
+	if err != nil {
+		domain.ErrorWarn(err)
+		return
+	}
+	rows.Next()
+	err = rows.Scan(
+		&a.ID, &a.Slug, &a.ArticleType, &a.Abstract, &a.Content, &a.Image,
+		&a.Author, &a.CreatedAt, &a.UpdatedAt,
+	)
+	if err != nil {
+		domain.ErrorWarn(err)
+		return
+	} else {
+		// if cs, err := artr.FilterCharaById(s); err != nil {
+		// 	domain.ErrorWarn(err)
+		// } else {
+		// 	a.Characters = cs
+		// }
 	}
 	return
 }
@@ -101,7 +156,13 @@ func (artr *ArticleRepository) Update(a domain.ArticleInput, articleId int) (aff
 	return int(rawAffected), err
 }
 
-func (artr *ArticleRepository) FilterCharaById(articleId int) (charas []domain.ArticleCharacter, err error) {
+// func (artr *ArticleRepository) Delete(id int) (affected int, err error) {
+// }
+
+// func (artr *ArticleRepository) FilterByAnime(animeId int) (as []domain.Article, err error) {
+// }
+
+func (artr *ArticleRepository) FilterCharaById(articleId int) (cs []domain.ArticleCharacter, err error) {
 	rows, err := artr.Query(
 		"SELECT id, chara_name, image, created_at, updated_at ",
 		"FROM anime_character ",
@@ -114,7 +175,25 @@ func (artr *ArticleRepository) FilterCharaById(articleId int) (charas []domain.A
 	for rows.Next() {
 		var c domain.ArticleCharacter
 		rows.Scan(&c.ID, &c.Name, &c.Image, &c.CreatedAt, &c.UpdatedAt)
-		charas = append(charas, c)
+		cs = append(cs, c)
 	}
 	return
 }
+
+// func (artr *ArticleRepository) FilterCharaByUserId(userId string) (cs []domain.ArticleCharacter, err error) {
+// }
+
+// func (artr *ArticleRepository) InsertChara(ci domain.ArticleCharacterInput) (inserted int, err error) {
+// }
+
+// func (artr *ArticleRepository) UpdateChara(ci domain.ArticleCharacterInput, id int) (affected int, err error) {}
+
+// func (artr *ArticleRepository) DeleteChara(id int) (affected int, err error) {}
+
+// func (artr *ArticleRepository) FetchInterview(articleId int) (ints []domain.InterviewQuote, err error) {}
+
+// func (artr *ArticleRepository) InsertInterview(ii domain.InterviewQuoteInput) (int, error)
+
+// func (artr *ArticleRepository) UpdateInterview(ii domain.InterviewQuoteInput, id int) (int, error) {}
+
+// func (artr *ArticleRepository) DeleteInterview(id int) (int, error) {}
