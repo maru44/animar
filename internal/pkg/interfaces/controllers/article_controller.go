83d751a0b1e6198f1346b0bb29245b04a55b7cf2
maru44 <takumaru22244@gmail.com>
2021-09-22 22:05:51 +0900

    article controller

--- internal/pkg/interfaces/controllers/article_controller.go
+++ internal/pkg/interfaces/controllers/article_controller.go
@@ -272,23 +272,38 @@ func (artc *ArticleController) InsertRelationArticleCharacterView(w http.Respons
 	return
 }
 
-// type ArticleRepository interface {
-// 	Fetch() ([]domain.Article, error)
-// 	GetById(id int) (domain.Article, error)
-// 	GetBySlug(slug string) (domain.Article, error)
-// 	Insert(articleInput domain.ArticleInput, userId string) (int, error)
-// 	Update(articleInput domain.ArticleInput, id int, userId string) (int, error)
-// 	Delete(id int) (int, error)
-// 	FilterByAnime(animeId int) ([]domain.Article, error)
-// 	FilterCharaById(articleId int) ([]domain.ArticleCharacter, error)
-// 	FilterCharaByUserId(userId string) ([]domain.ArticleCharacter, error)
-// 	InsertChara(charaInput domain.ArticleCharacterInput, animeId int, userId string) (int, error)
-// 	UpdateChara(charaInput domain.ArticleCharacterInput, id int, userId string) (int, error)
-// 	DeleteChara(id int) (int, error)
-// 	FetchInterview(articleId int) ([]domain.InterviewQuote, error)
-// 	InsertInterview(interviewInput domain.InterviewQuoteInput, userId string) (int, error)
-// 	UpdateInterview(interviewInput domain.InterviewQuoteInput, id int) (int, error)
-// 	DeleteInterview(id int) (int, error)
+func (artc *ArticleController) DeleteRelationArticleCharacterView(w http.ResponseWriter, r *http.Request) {
+	var in domain.RelationArticleCharacterInput
+	if err := json.NewDecoder(r.Body).Decode(&in); err != nil {
+		response(w, err, nil)
+		return
+	}
+
+	affected, err := artc.interactor.DeleteRelationArticleCharacter(in)
+	if err != nil {
+		response(w, err, nil)
+	} else {
+		response(w, err, map[string]interface{}{"data": affected})
+	}
+	return
+}
+
+func (artc *ArticleController) InsertRelationArticleAnimeView(w http.ResponseWriter, r *http.Request) {
+	var in domain.RelationArticleAnimeInput
+	if err := json.NewDecoder(r.Body).Decode(&in); err != nil {
+		response(w, err, nil)
+		return
+	}
+
+	inserted, err := artc.interactor.InsertRelationArticleAnime(in)
+	if err != nil {
+		response(w, err, nil)
+	} else {
+		response(w, err, map[string]interface{}{"data": inserted})
+	}
+	return
+}
+
 // 	InsertRelationArticleCharacter(in domain.RelationArticleCharacterInput) (int, error)
 // 	DeleteRelationArticleCharacter(in domain.RelationArticleCharacterInput) (int, error)
 // 	InsertRelationArticleAnime(in domain.RelationArticleAnimeInput) (int, error)
