3da669ae1521c5f5ae32da717ab6201969ae373c
maru44 <takumaru22244@gmail.com>
2021-09-24 22:36:24 +0900

    modify errors

--- internal/pkg/domain/errors.go
+++ internal/pkg/domain/errors.go
@@ -2,6 +2,7 @@ package domain
 
 import "errors"
 
+// for response
 var (
 	ErrInternalServerError = errors.New("Internal Server Error")
 	ErrNotFound            = errors.New("Not Found")
@@ -13,3 +14,66 @@ var (
 	ErrCsrfNotValid        = errors.New("Csrf Not Valid")
 	StatusCreated          = errors.New("Created")
 )
+
+const (
+	/*  external  */
+
+	ExternalServerError uint32 = 1 << iota
+	DataNotFoundError
+	TokenIsInvalidError
+	TokenIsExpiredError
+	UnauthorizedError
+	ForbiddenError
+
+	/*  Internal not emergency  */
+	InternalServerError
+
+	/*  Internal Emergency  */
+
+	MySqlConnectionError
+	FirebaseConnectionError
+	S3ConnectionError
+	HttpConnectionError
+)
+
+type (
+	Error struct {
+		Inner error // stores the error returned by external dependencies
+		Flag  uint32
+		text  string
+	}
+)
+
+func (e Error) Errors() string {
+	if e.Inner != nil {
+		return e.Inner.Error()
+	} else if e.text != "" {
+		return e.text
+	} else {
+		return ErrInternalServerError.Error()
+	}
+}
+
+func (e Error) ErrorForOutput() error {
+	switch e.Flag {
+	case ExternalServerError:
+		return ErrBadRequest
+	case UnauthorizedError, TokenIsExpiredError, TokenIsInvalidError:
+		return ErrUnauthorized
+	case ForbiddenError:
+		return ErrForbidden
+	case DataNotFoundError:
+		return ErrNotFound
+	case MySqlConnectionError, FirebaseConnectionError, S3ConnectionError, HttpConnectionError:
+		return ErrInternalServerError
+	default:
+		return ErrInternalServerError
+	}
+}
+
+func NewError(text string, flag uint32) *Error {
+	return &Error{
+		Flag: flag,
+		text: text,
+	}
+}
