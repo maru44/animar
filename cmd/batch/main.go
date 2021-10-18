510d5bccd92a587580a0b686b0bc9502007f3e6f
maru44 <takumaru22244@gmail.com>
2021-10-19 05:45:12 +0900

    batch separate

--- cmd/batch/main.go
+++ cmd/batch/main.go
@@ -1,21 +1,10 @@
 package main
 
 import (
-	"animar/v1/internal/pkg/infrastructure"
-	"animar/v1/internal/pkg/interfaces/batch"
-	"log"
+	"animar/v1/internal/pkg/batch/dbbackup"
 )
 
 func main() {
-	// dbbackup.Test()
-	sqlHandler := infrastructure.NewSqlHandler()
-
-	platformBatch := batch.NewPlatformBatch(sqlHandler)
-
-	if err := platformBatch.SendBatch(); err != nil {
-		log.Println(err)
-	}
-
 	// @TODO remove comment out
-	// dbbackup.BackupMainDatabase()
+	dbbackup.BackupMainDatabase()
 }
